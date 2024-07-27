9e4c6ddb143d78121c7b0723ddfc827594dae8ac
huipukui <huipukui@autohome.com.cn>
2024-07-27 10:34:22 +0800

    add 01Xcode

---
+++ 01Xcode/01Xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01Xcode
+//
+//  Created by HuiPuKui on 2024/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            .font(.title)
+            .fontWeight(.black)
+            .foregroundColor(Color.green)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01Xcode/01Xcode/_1XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1XcodeApp.swift
+//  01Xcode
+//
+//  Created by HuiPuKui on 2024/7/27.
+//
+
+import SwiftUI
+
+@main
+struct _1XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
